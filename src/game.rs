0d61b832a98ef31655f71ea082b325b48cda73c1
jonasskounielsen <jonasskou.n@gmail.com>
2026-03-13 15:17:26 +0100

    Draw from main menu state struct

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,4 @@
-use nannou::Draw;
+use nannou::{Draw, color::rgb};
 
 use crate::game::level::Level;
 
@@ -6,28 +6,73 @@ mod level;
 
 #[derive(Debug)]
 pub enum Game {
-    MainMenu,
-    InLevel {
-        level: Level,
-    },
-    WonLevel {
-        level: Level,
-    },
-    LostLevel {
-        level: Level,
-    },
+    MainMenu (MainMenu),
+    InLevel  (InLevel),
+    WonLevel (WonLevel),
+    LostLevel(LostLevel),
+}
+
+#[derive(Debug)]
+pub struct MainMenu;
+
+impl MainMenu {
+    pub fn new() -> Self {
+        MainMenu
+    }
+
+    pub fn draw(&self, draw: &Draw) {
+        draw.background().color(rgb(0.0, 255.0, 0.0));
+    }
+}
+
+#[derive(Debug)]
+pub struct InLevel {
+    level: Level,
+}
+
+impl InLevel {
+    pub fn draw(&self, draw: &Draw) {
+
+    }
+}
+
+#[derive(Debug)]
+pub struct WonLevel {
+    level: Level,
+}
+
+impl WonLevel {
+    pub fn draw(&self, draw: &Draw) {
+
+    }
+}
+
+#[derive(Debug)]
+pub struct LostLevel {
+    level: Level,
+}
+
+impl LostLevel {
+    pub fn draw(&self, draw: &Draw) {
+
+    }
 }
 
 impl Game {
     pub fn new() -> Self {
-        Self::MainMenu
+        Self::MainMenu(MainMenu::new())
     }
 
     pub fn tick() {
 
     }
 
-    pub fn draw(draw: Draw) {
-        draw.background().color(nannou::color::AQUAMARINE);
+    pub fn draw(&self, draw: &Draw) {
+        match self {
+            Self::MainMenu (main_menu)    => main_menu .draw(draw),
+            Self::InLevel  (in_level)      => in_level  .draw(draw),
+            Self::WonLevel (won_level)    => won_level .draw(draw),
+            Self::LostLevel(lost_level)  => lost_level.draw(draw),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
-mod nannou;
+use nannou::prelude::*;
+use crate::game::Game;
+
 mod game;
 
-fn main() {
-    nannou::run();
+pub fn main() {
+    nannou::app(model).update(update).run();
+}
+
+fn model(app: &App) -> Game {
+    app.new_window()
+        .size(400, 400)
+        .view(view)
+        .build()
+        .unwrap();
+
+    Game::new()
+}
+
+fn update(_app: &App, game: &mut Game, _update: Update) {
+    
+}
+
+fn view(app: &App, game: &Game, frame: Frame) {
+    let draw = app.draw();
+
+    game.draw(&draw);
+
+    draw.to_frame(app, &frame).unwrap();
 }
